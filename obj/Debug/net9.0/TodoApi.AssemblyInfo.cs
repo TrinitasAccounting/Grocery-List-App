635af82a4445e09044119f8aa5a10a4e8f177f21
Clay Mangum <clay@trinitasaccounting.com>
2025-01-14 16:40:17 -0500

    fully functioning ASP.net API

--- obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f39a3b1a1fb2402863a546dfeb85cf0553f0e27b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3101ca318633cf59e021dca9016fce480974578d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
