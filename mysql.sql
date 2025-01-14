1d498d532651bcb38366b6d215129e6bd0ae995c
Clay Mangum <clay@trinitasaccounting.com>
2025-01-14 17:07:54 -0500

    added working SQLite database to app

---
+++ mysql.sql
@@ -0,0 +1,8 @@
+
+
+-- CREATE TABLE todos(todoid INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, isComplete NOT NULL);
+
+-- INSERT INTO todos(todoid, name, isComplete)
+-- VALUES (3,"Walk Dog", False);
+
+-- SELECT * FROM todos;
